863dbfd544f00a8b9aadb3ad0152ab8069fe4e03
Chanhwi Lee <chanhwi.lee6@gmail.com>
2022-08-17 23:37:20 +0900

    Write 1

---
+++ README.md
@@ -0,0 +1,5 @@
+## 5.2
+
+    기본적인 Navigation
+    Wirte 컴포넌트에 TextInput 사용법, onSubmitEditing 등 숙지
+    State 에 feeling(TextInput), Emoge 넣음
--- screens/Write.js
+++ screens/Write.js
@@ -1,17 +1,100 @@
-import React from "react";
+import React, { useState } from "react";
+import { Alert } from "react-native";
 import styled from "styled-components/native";
+import colors from "../colors";
 
 const View = styled.View`
   flex: 1;
-  justify-content: center;
-  align-items: center;
+  padding: 0px 30px;
+  background-color: ${colors.bgColor};
 `;
-const Text = styled.Text``;
+const Title = styled.Text`
+  color: ${colors.textColor};
+  margin: 50px 0px;
+  text-align: center;
+  font-size: 26px;
+  font-weight: 500;
+`;
+
+const TextInput = styled.TextInput`
+  background-color: white;
+  border-radius: 20px;
+  padding: 10px 20px;
+  font-size: 16px;
+`;
+
+const Btn = styled.TouchableOpacity`
+  width: 100%;
+  margin-top: 25px;
+  padding: 10px 20px;
+  background-color: ${colors.btnColor};
+  align-items: center;
+  border-radius: 20px;
+`;
+
+const BtnText = styled.Text`
+  color: white;
+  font-size: 16px;
+  font-weight: 500;
+`;
+
+const Emotions = styled.View`
+  flex-direction: row;
+  margin-bottom: 20px;
+  justify-content: space-between;
+`;
+
+const Emotion = styled.TouchableOpacity`
+  background-color: white;
+  box-shadow: 1px 1px 5px rgba(0, 0, 0, 0.1);
+  padding: 10px;
+  border-radius: 20px;
+  overflow: hidden;
+  border: ${(props) => (props.selected ? "2px" : "0px")};
+  border-color: rgba(0, 0, 0, 0.5);
+`;
+
+const EmotionText = styled.Text`
+  font-size: 22px;
+`;
+
+const emotions = ["🤯", "🥲", "🤬", "🤗", "🥰", "😊", "🤩"];
 
 const Write = () => {
+  const [selectedEmotion, setEmotion] = useState(null);
+  const [feelings, setFeelings] = useState("");
+  const onEmotionPress = (face) => setEmotion(face);
+  const onChangeText = (text) => setFeelings(text);
+  const onSubmit = () => {
+    if (feelings === "" || selectedEmotion == null) {
+      return Alert.alert("다시 입력해주세요~");
+    }
+  };
+
   return (
     <View>
-      <Text>Write</Text>
+      <Title>오늘 기분이 어때요?</Title>
+      <Emotions>
+        {emotions.map((emotion, index) => (
+          <Emotion
+            selected={emotion === selectedEmotion}
+            key={index}
+            onPress={() => onEmotionPress(emotion)}
+          >
+            <EmotionText>{emotion}</EmotionText>
+          </Emotion>
+        ))}
+      </Emotions>
+      <TextInput
+        returnKeyLabel="done"
+        onSubmitEditing={onSubmit}
+        placeholder="오늘의 기분을 적어보세요"
+        value={feelings}
+        onChangeText={onChangeText}
+      />
+      <Btn onPress={onSubmit}>
+        <BtnText>저장</BtnText>
+      </Btn>
     </View>
   );
 };
